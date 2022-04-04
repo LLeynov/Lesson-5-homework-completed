38ef42bed1233b3a5f17dcb0bfd4e2abcb568d73
Lev <lleynov2018@mail.ru>
2022-04-05 00:06:26 +0300

    Выполнен второй пункт домашнего задания.

--- Lesson 5 homework/Program.cs
+++ Lesson 5 homework/Program.cs
@@ -4,12 +4,20 @@ using System.IO;
 
 namespace Lesson_5_homework
 {
+    //Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filename = "text.txt";
-            File.WriteAllText(filename, Console.ReadLine()); // записываем в файл строку
+            string filename = "statrup.txt";
+            
+            File.WriteAllText(filename,"Текущая дата и время : " + DateTime.Now + "\nВаш текст : " + Console.ReadLine()); 
+            //Первой строчкой выводим время и дату,второй заданный текст. 
+            
+            
+
         }
     }
 }
